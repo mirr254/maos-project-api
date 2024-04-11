6b78a3a7e1d935ad5cc9df10bd4560a124ebeb04
samuel kungu <sammysteppa90@gmail.com>
2024-04-11 22:29:00 +0100

    Fix build workflow (#15)
    * email validation
    
    * install pulumi
    
    * install deps
    
    * fixes docker compose install

--- models/User.go
+++ models/User.go
@@ -2,12 +2,16 @@ package models
 
 import (
 	"gorm.io/gorm"
+	"time"
 )
 
 type User struct {
 	gorm.Model
-	Name     string `json:"name"`
-	Email    string `json:"email" gorm:"unique"` 
-	Password string `json:"password"`
-	Role     string `json:"role"`
+	Name                 string    `json:"name"`
+	Email                string    `json:"email" gorm:"unique"`
+	Password             string    `json:"password"`
+	Role                 string    `json:"role"`
+	IsEmailVerified      bool      `json:"is_email_verified"`
+	EmailVerificationToken string    `json:"email_verification_token"`
+	EmailTokenGeneratedAt time.Time `json:"email_token_generated_at"`
 }
