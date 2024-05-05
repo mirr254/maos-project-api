fd98bc9166de9053eea63ad2e474992feeb75c03
samuel kungu <sammysteppa90@gmail.com>
2024-05-05 17:02:39 +0100

    Fix failing tests (#20)
    * mock working
    token generation is different
    
    * fixed all failing tests
    
    * clear users table on suite setup
    
    * comment

--- models/User.go
+++ models/User.go
@@ -4,13 +4,13 @@ import (
 	"gorm.io/gorm"
 )
 
-type User struct {
+type Users struct {
 	gorm.Model
-	Name                 string    `json:"name"`
-	Email                string    `json:"email" gorm:"unique"`
-	Password             string    `json:"password"`
-	Role                 string    `json:"role"`
-	IsEmailVerified      bool      `json:"is_email_verified"`
+	Name                   string    `json:"name"`
+	Email                  string    `json:"email" gorm:"unique"`
+	Password               string    `json:"password"`
+	Role                   string    `json:"role"`
+	IsEmailVerified        bool      `json:"is_email_verified" gorm:"default:false"`
 	EmailVerificationToken string    `json:"email_verification_token"`
-	ResetPasswordToken   string    `json:"reset_password_token"`
+	ResetPasswordToken     string    `json:"reset_password_token"`
 }
