bc28e643e96e7b43111abfac4e121451ea8d1f48
mirr254 <sammysteppa90@gmail.com>
2024-06-26 14:54:49 +0100

    add stack operations in router

--- router/project.go
+++ router/project.go
@@ -11,5 +11,7 @@ func ProjectRoutes( r *gin.Engine) {
 
 	v1.POST("/project", controllers.CreateProject)
 	v1.POST("/stack", controllers.CreateStack)
+	v1.GET("/stack", controllers.GetStack)
+	v1.POST("/stack/destroy", controllers.DeleteStack)
 
 }
